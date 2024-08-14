712f439ea9ba3193ccd00fe0c6141826b91ab817
idnc_sk <me@idnc.sk>
2024-08-14 22:20:16 +0200

    aaaa

--- src/core/index.js
+++ src/core/index.js
@@ -3,6 +3,91 @@
 
 /*
 
+index
+    .list()
+    .has()
+    .get()
+    .set()
+    .delete()
+    .clear()
+    .entries()
+    .size()
+    .create()
+    .destroy()
+    .db
+
+
+index.tree
+index.layers
+
+index.sessions
+
+index.devices
+index.apps
+index.roles
+index.identities
+
+? index.users
+
+db.documents
+
+---
+bitmaps
+    context
+        global
+        user
+    features
+        data/
+        app/
+        custom/
+    filters
+        date
+        time
+        name
+
+hashmaps
+    sha1
+        hash | id
+    sha256
+        hash | id
+    xxhash
+        hash | id
+
+vectors
+    id | vector
+metadata
+    id | meta
+
+    SynapseDB
+
+Bitmap indexes
+ - Context
+    - Global/System
+        - DeviceID (uuid12 of the current device)
+        - Network (172.16.2.0/24)
+        - Current os.user
+    - User
+        - Global context tree
+
+ - Features
+    - builtin (set intrinsically/gathered by the ftExtract() function)
+        - data/abstraction/{tab,note,file,email..}
+        - mime/type/application-json
+    - builtin/nested
+        - data/abstraction/email/from/contactID
+        - data/abstraction/file/ext/json
+    - custom
+        - custom/tag/<tag>
+        - custom/app/<appid>
+    - custom/nested
+        - custom/user/userid
+        - custom/device/deviceid
+
+ - Filters
+    - date/20240101
+    -
+
+
 canvas.paths() // Return the current canvas paths for user and server
 canvas.paths('user') // Return the current canvas paths for user
 canvas.paths('server') // Return the current canvas paths for server
