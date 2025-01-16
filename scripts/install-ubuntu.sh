c9f2f28e4237d4f64ec4a519cfbecfb95105209f
idnc_sk <me@idnc.sk>
2025-01-16 17:54:13 +0100

    Adding deployment scripts .1

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -37,6 +37,8 @@ setup_nodejs_repository() {
 
 	rm -f /usr/share/keyrings/nodesource.gpg || true
     rm -f /etc/apt/sources.list.d/nodesource.list || true
+	rm -f /etc/apt/preferences.d/nsolid || true
+	rm -f /etc/apt/preferences.d/nodejs || true
 
     # Run 'curl' and 'gpg' to download and import the NodeSource signing key
     if ! curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /usr/share/keyrings/nodesource.gpg; then
@@ -49,7 +51,7 @@ setup_nodejs_repository() {
         handle_error "$?" "Failed to set correct permissions on /usr/share/keyrings/nodesource.gpg"
     fi
 
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODEJS_VERSION nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODEJS_VERSION.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
 
     # N|solid Config
     echo "Package: nsolid" | tee /etc/apt/preferences.d/nsolid > /dev/null
