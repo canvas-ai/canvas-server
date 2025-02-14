e2498aeb231416c36f59fcfde5fbf34fa230b043
Tarbor <tarborlevan@gmail.com>
2025-02-14 09:31:49 +0400

    move path resolver files into src folder

--- loader.js
+++
@@ -1,8 +0,0 @@
-import { register } from 'node:module'
-import { pathToFileURL } from 'node:url'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-register('./register-paths.js', pathToFileURL('./'))
