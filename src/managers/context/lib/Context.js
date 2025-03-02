3f9a74f5fea06167f70c93f258e279c295831b8a
idnc_sk <me@idnc.sk>
2025-03-02 14:02:44 +0100

    refactoring ai generated code mixture of old and new to a human readable format v1.2, have to go

--- src/managers/context/lib/Context.js
+++ src/managers/context/lib/Context.js
@@ -83,7 +83,6 @@ class Context extends EventEmitter {
     get identity() { return this.#user.identity; }
     get tree() { return this.#workspace.tree.toJSON(); } // Legacy
 
-
     /**
      * Context API
      */
@@ -97,27 +96,163 @@ class Context extends EventEmitter {
      * Document API
      */
 
-    getDocument(documentId) {
-        if (!this.#workspace || !this.#workspace.db) {
-            throw new Error('Workspace or database not available');
-        }
-
-    }
-
-    getDocuments(featureArray = [], filterArray = [], options = {}) {
-        if (!this.#workspace || !this.#workspace.db) {
-            throw new Error('Workspace or database not available');
-        }
-    }
-
     listDocuments(featureArray = [], filterArray = [], options = {}) {
         if (!this.#workspace || !this.#workspace.db) {
             throw new Error('Workspace or database not available');
         }
+
+        // This may be subject to change, if featureArray is empty, we'll use the context-wide feature array
+        if (featureArray.length === 0) {
+            featureArray = this.#featureBitmapArray;
+        }
+
+        // Additionally filter based on system features (os, app, device, user)
+        if (options.includeSystemContext) {
+            featureArray.push(this.#systemBitmapArray);
+        }
+
+        // Filters are out of scope for now
+        return this.#db.listDocuments(featureArray, filterArray, options);
     }
+
+    insertDocument(document, featureArray = []) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        if (!document) {
+            throw new Error('Document is required');
+        }
+
+        // We always index with all features
+        featureArray = [
+            ...this.#featureBitmapArray,
+            ...this.#systemBitmapArray,
+            ...featureArray,
+        ];
+
+        // Insert the document
+        return this.#db.insertDocument(document, featureArray, options); // not sure if its a good practice to allow passing options here
+    }
+
+    insertDocuments(documentArray, featureArray = [], options = {}) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        if (!Array.isArray(documentArray)) {
+            throw new Error('Document array must be an array');
+        }
+
+        // We always index with all features
+        featureArray = [
+            ...this.#featureBitmapArray,
+            ...this.#systemBitmapArray,
+            ...featureArray,
+        ];
+
+        // Insert the documents
+        return this.#db.insertDocuments(documentArray, featureArray, options);
+    }
+
+    updateDocument(document, featureArray = []) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        if (!document) {
+            throw new Error('Document is required');
+        }
+
+        // We always index with all features
+        featureArray = [
+            ...this.#featureBitmapArray,
+            ...this.#systemBitmapArray,
+            ...featureArray,
+        ];
+
+        // Update the document
+        return this.#db.updateDocument(document, featureArray, options);
+    }
+
+    updateDocuments(documentArray, featureArray = [], options = {}) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        if (!Array.isArray(documentArray)) {
+            throw new Error('Document array must be an array');
+        }
+
+        // Update the documents
+        return this.#db.updateDocuments(documentArray, featureArray, options);
+    }
+
+    removeDocument(documentId, options = {}) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        // We remove document from the current context not from the database
+        return this.#db.removeDocument(documentId, featureArray, options);
+    }
+
+    removeDocuments(documentIdArray, featureArray = [], options = {}) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        if (!Array.isArray(documentIdArray)) {
+            throw new Error('Document ID array must be an array');
+        }
+
+        // We remove documents from the current context not from the database
+        return this.#db.removeDocuments(documentIdArray, featureArray, options);
+    }
+
+    deleteDocument(documentId) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        // Completely delete the document from the database
+        return this.#db.deleteDocument(documentId);
+    }
+
+    deleteDocuments(documentIdArray, featureArray = [], options = {}) {
+        if (!this.#workspace || !this.#workspace.db) {
+            throw new Error('Workspace or database not available');
+        }
+
+        if (!Array.isArray(documentIdArray)) {
+            throw new Error('Document ID array must be an array');
+        }
+
+        // Completely delete the documents from the database
+        return this.#db.deleteDocuments(documentIdArray, featureArray, options);
+    }
+
+    /**
+     * Document Feature API
+     */
+
+    addDocumentFeature(feature) { }
+    removeDocumentFeature(feature) { }
+    listDocumentFeatures() { }
+    hasDocumentFeature(feature) { }
+
+    /**
+     * Utils
+     */
 
     toJSON() {
         return {
+            id: this.#id,
+            name: this.#name,
+            url: this.#url,
+            baseUrl: this.#baseUrl,
+            created: this.#created,
+            updated: this.#updated,
         };
     }
 
